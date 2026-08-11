b3f739860052fee4e44f5384c26b7991ce03dc73
joaords98-ctrl <joaords98@gmail.com>
2026-08-11 18:24:29 -0300

    Add files via upload

---
+++ gerador/candidatos.js
@@ -0,0 +1,465 @@
+window.GERADOR_CANDIDATOS = [
+  {
+    "name": "Adriano Fritz",
+    "role": "Deputado Estadual",
+    "number": "14042",
+    "slug": "adriano-fritz",
+    "photo": "../assets/candidatos/adriano-fritz.jpg"
+  },
+  {
+    "name": "Alessandra Lima",
+    "role": "Deputada Estadual",
+    "number": "14039",
+    "slug": "alessandra-lima",
+    "photo": "../assets/candidatos/alessandra-lima.jpg"
+  },
+  {
+    "name": "Aline Franzon",
+    "role": "Deputada Estadual",
+    "number": "14143",
+    "slug": "aline-franzon",
+    "photo": "../assets/candidatos/aline-franzon.jpg"
+  },
+  {
+    "name": "Beatriz Oliveira",
+    "role": "Deputada Estadual",
+    "number": "14441",
+    "slug": "beatriz-oliveira",
+    "photo": "../assets/candidatos/beatriz-oliveira.jpg"
+  },
+  {
+    "name": "Bruno Vieira",
+    "role": "Deputado Estadual",
+    "number": "14777",
+    "slug": "bruno-vieira",
+    "photo": "../assets/candidatos/bruno-vieira.jpg"
+  },
+  {
+    "name": "Carlos Eduardo",
+    "role": "Deputado Estadual",
+    "number": "14007",
+    "slug": "carlos-eduardo",
+    "photo": "../assets/candidatos/carlos-eduardo.jpg"
+  },
+  {
+    "name": "Daniel Aguiar",
+    "role": "Deputado Estadual",
+    "number": "14321",
+    "slug": "daniel-aguiar",
+    "photo": "../assets/candidatos/daniel-aguiar.jpg"
+  },
+  {
+    "name": "Danusa Lopes",
+    "role": "Deputada Estadual",
+    "number": "14077",
+    "slug": "danusa-lopes",
+    "photo": "../assets/candidatos/danusa-lopes.jpg"
+  },
+  {
+    "name": "Dyamond Pires",
+    "role": "Deputado Estadual",
+    "number": "14141",
+    "slug": "dyamond-pires",
+    "photo": "../assets/candidatos/dyamond-pires.jpg"
+  },
+  {
+    "name": "Eduardo Gomes",
+    "role": "Deputado Estadual",
+    "number": "14021",
+    "slug": "eduardo-gomes",
+    "photo": "../assets/candidatos/eduardo-gomes.jpg"
+  },
+  {
+    "name": "Erica Gonçalves",
+    "role": "Deputada Estadual",
+    "number": "14011",
+    "slug": "erica-goncalves",
+    "photo": "../assets/candidatos/erica-goncalves.jpg"
+  },
+  {
+    "name": "Felipe de Castilho",
+    "role": "Deputado Estadual",
+    "number": "14111",
+    "slug": "felipe-de-castilho",
+    "photo": "../assets/candidatos/felipe-de-castilho.jpg"
+  },
+  {
+    "name": "Francyelly Engee",
+    "role": "Deputada Estadual",
+    "number": "14214",
+    "slug": "francyelly-engee",
+    "photo": "../assets/candidatos/francyelly-engee.jpg"
+  },
+  {
+    "name": "Graci da Faúna Silvestre",
+    "role": "Deputada Estadual",
+    "number": "14900",
+    "slug": "graci-da-fauna-silvestre",
+    "photo": "../assets/candidatos/graci-da-fauna-silvestre.jpg"
+  },
+  {
+    "name": "Graci Moreira",
+    "role": "Deputada Estadual",
+    "number": "14714",
+    "slug": "graci-moreira",
+    "photo": "../assets/candidatos/graci-moreira.jpg"
+  },
+  {
+    "name": "Gustavo Camillo",
+    "role": "Deputado Estadual",
+    "number": "14000",
+    "slug": "gustavo-camillo",
+    "photo": "../assets/candidatos/gustavo-camillo.jpg"
+  },
+  {
+    "name": "Ivo Arthur",
+    "role": "Deputado Estadual",
+    "number": "14222",
+    "slug": "ivo-arthur",
+    "photo": "../assets/candidatos/ivo-arthur.jpg"
+  },
+  {
+    "name": "João Gabriel",
+    "role": "Deputado Estadual",
+    "number": "14144",
+    "slug": "joao-gabriel",
+    "photo": "../assets/candidatos/joao-gabriel.jpg"
+  },
+  {
+    "name": "Kelven Alcantara",
+    "role": "Deputado Estadual",
+    "number": "14500",
+    "slug": "kelven-alcantara",
+    "photo": "../assets/candidatos/kelven-alcantara.jpg"
+  },
+  {
+    "name": "Lucas Backes",
+    "role": "Deputado Estadual",
+    "number": "14446",
+    "slug": "lucas-backes",
+    "photo": "../assets/candidatos/lucas-backes.jpg"
+  },
+  {
+    "name": "Lude Riesemberg",
+    "role": "Deputado Estadual",
+    "number": "14041",
+    "slug": "lude-riesemberg",
+    "photo": "../assets/candidatos/lude-riesemberg.jpg"
+  },
+  {
+    "name": "Mari Mazzon",
+    "role": "Deputada Estadual",
+    "number": "14444",
+    "slug": "mari-mazzon",
+    "photo": "../assets/candidatos/mari-mazzon.jpg"
+  },
+  {
+    "name": "Mateus Pepice",
+    "role": "Deputado Estadual",
+    "number": "14131",
+    "slug": "mateus-pepice",
+    "photo": "../assets/candidatos/mateus-pepice.jpg"
+  },
+  {
+    "name": "Matheus Manholer",
+    "role": "Deputado Estadual",
+    "number": "14333",
+    "slug": "matheus-manholer",
+    "photo": "../assets/candidatos/matheus-manholer.jpg"
+  },
+  {
+    "name": "Natalie Facco",
+    "role": "Deputada Estadual",
+    "number": "14114",
+    "slug": "natalie-facco",
+    "photo": "../assets/candidatos/natalie-facco.jpg"
+  },
+  {
+    "name": "Odenilson Vicente",
+    "role": "Deputado Estadual",
+    "number": "14100",
+    "slug": "odenilson-vicente",
+    "photo": "../assets/candidatos/odenilson-vicente.jpg"
+  },
+  {
+    "name": "Paulo Di Melo",
+    "role": "Deputado Estadual",
+    "number": "14999",
+    "slug": "paulo-di-melo",
+    "photo": "../assets/candidatos/paulo-di-melo.jpg"
+  },
+  {
+    "name": "Raphael Lacerda",
+    "role": "Deputado Estadual",
+    "number": "14914",
+    "slug": "raphael-lacerda",
+    "photo": "../assets/candidatos/raphael-lacerda.jpg"
+  },
+  {
+    "name": "Rubens Nascimento",
+    "role": "Deputado Estadual",
+    "number": "14123",
+    "slug": "rubens-nascimento",
+    "photo": "../assets/candidatos/rubens-nascimento.jpg"
+  },
+  {
+    "name": "Stuchi",
+    "role": "Deputada Estadual",
+    "number": "14244",
+    "slug": "stuchi",
+    "photo": "../assets/candidatos/stuchi.jpg"
+  },
+  {
+    "name": "Thiago Maldonado",
+    "role": "Deputado Estadual",
+    "number": "14001",
+    "slug": "thiago-maldonado",
+    "photo": "../assets/candidatos/thiago-maldonado.jpg"
+  },
+  {
+    "name": "Wagner Jr.",
+    "role": "Deputado Estadual",
+    "number": "14555",
+    "slug": "wagner-jr",
+    "photo": "../assets/candidatos/wagner-jr.jpg",
+    "fallback": "./assets/wagner-jr.jpg"
+  },
+  {
+    "name": "Willian Rocha",
+    "role": "Deputado Estadual",
+    "number": "14014",
+    "slug": "willian-rocha",
+    "photo": "../assets/candidatos/willian-rocha.jpg"
+  },
+  {
+    "name": "Alan Leal",
+    "role": "Deputado Federal",
+    "number": "1404",
+    "slug": "alan-leal",
+    "photo": "../assets/candidatos/alan-leal.jpg"
+  },
+  {
+    "name": "Ana Lúcia",
+    "role": "Deputada Federal",
+    "number": "1439",
+    "slug": "ana-lucia",
+    "photo": "../assets/candidatos/ana-xavier.jpg"
+  },
+  {
+    "name": "André Ribeiro",
+    "role": "Deputado Federal",
+    "number": "1450",
+    "slug": "andre-ribeiro",
+    "photo": "../assets/candidatos/andre-ribeiro.jpg"
+  },
+  {
+    "name": "Andrey Luiz",
+    "role": "Deputado Federal",
+    "number": "1416",
+    "slug": "andrey-luiz",
+    "photo": "../assets/candidatos/andrey-luiz.jpg"
+  },
+  {
+    "name": "Anita de Biasi",
+    "role": "Deputada Federal",
+    "number": "1419",
+    "slug": "anita-de-biasi",
+    "photo": "../assets/candidatos/anita-de-biasi.jpg"
+  },
+  {
+    "name": "Carlos Gasparin",
+    "role": "Deputado Federal",
+    "number": "1411",
+    "slug": "carlos-gasparin",
+    "photo": "../assets/candidatos/carlos-gasparin.jpg"
+  },
+  {
+    "name": "Caroline Barbosa",
+    "role": "Deputada Federal",
+    "number": "1497",
+    "slug": "caroline-barbosa",
+    "photo": "../assets/candidatos/caroline-barbosa.jpg"
+  },
+  {
+    "name": "Darlon Paraná Pop",
+    "role": "Deputado Federal",
+    "number": "1444",
+    "slug": "darlon-parana-pop",
+    "photo": "../assets/candidatos/darlon-parana-pop.jpg"
+  },
+  {
+    "name": "Delegado Eduardo Kruger",
+    "role": "Deputado Federal",
+    "number": "1415",
+    "slug": "delegado-eduardo-kruger",
+    "photo": "../assets/candidatos/delegado-eduardo-kruger.jpg"
+  },
+  {
+    "name": "Dr. Guilherme Machado",
+    "role": "Deputado Federal",
+    "number": "1442",
+    "slug": "dr-guilherme-machado",
+    "photo": "../assets/candidatos/dr-guilherme-machado.jpg"
+  },
+  {
+    "name": "Dra. Yvis",
+    "role": "Deputada Federal",
+    "number": "1401",
+    "slug": "dra-yvis",
+    "photo": "../assets/candidatos/yvis-de-oliveira.jpg"
+  },
+  {
+    "name": "Felipe Gambaro",
+    "role": "Deputado Federal",
+    "number": "1420",
+    "slug": "felipe-gambaro",
+    "photo": "../assets/candidatos/felipe-gambaro.jpg"
+  },
+  {
+    "name": "Fernando Garbatchevski",
+    "role": "Deputado Federal",
+    "number": "1422",
+    "slug": "fernando-garbatchevski",
+    "photo": "../assets/candidatos/fernando-garbatchevski.jpg"
+  },
+  {
+    "name": "Gabriel Bertolucci",
+    "role": "Deputado Federal",
+    "number": "1400",
+    "slug": "gabriel-bertolucci",
+    "photo": "../assets/candidatos/gabriel-bertolucci.jpg"
+  },
+  {
+    "name": "Gerusa Correa",
+    "role": "Deputada Federal",
+    "number": "1477",
+    "slug": "gerusa-correa",
+    "photo": "../assets/candidatos/gerusa-correa.jpg"
+  },
+  {
+    "name": "Glauberson Rocha",
+    "role": "Deputado Federal",
+    "number": "1499",
+    "slug": "glauberson-rocha",
+    "photo": "../assets/candidatos/glauberson-rocha.jpg"
+  },
+  {
+    "name": "Jornalista Isaak Almeida",
+    "role": "Deputado Federal",
+    "number": "1456",
+    "slug": "jornalista-isaak-almeida",
+    "photo": "../assets/candidatos/jornalista-isaak-almeida.jpg"
+  },
+  {
+    "name": "Manu Shtorach",
+    "role": "Deputada Federal",
+    "number": "1457",
+    "slug": "manu-shtorach",
+    "photo": "../assets/candidatos/manu-shtorach.jpg"
+  },
+  {
+    "name": "Marcello de Paula",
+    "role": "Deputado Federal",
+    "number": "1407",
+    "slug": "marcello-de-paula",
+    "photo": "../assets/candidatos/marcello-de-paula.jpg"
+  },
+  {
+    "name": "Mari Guedes PCD",
+    "role": "Deputada Federal",
+    "number": "1406",
+    "slug": "mari-guedes-pcd",
+    "photo": "../assets/candidatos/mary-missao-pcd.jpg"
+  },
+  {
+    "name": "Mary Barreto",
+    "role": "Deputada Federal",
+    "number": "1430",
+    "slug": "mary-barreto",
+    "photo": "../assets/candidatos/mary-barreto.jpg"
+  },
+  {
+    "name": "Matheus Moreira",
+    "role": "Deputado Federal",
+    "number": "1443",
+    "slug": "matheus-moreira",
+    "photo": "../assets/candidatos/matheus-moreira.jpg"
+  },
+  {
+    "name": "Matheus Zozoloto",
+    "role": "Deputado Federal",
+    "number": "1410",
+    "slug": "matheus-zozoloto",
+    "photo": "../assets/candidatos/matheus-zozoloto.jpg"
+  },
+  {
+    "name": "Mauricio Franciscon",
+    "role": "Deputado Federal",
+    "number": "1446",
+    "slug": "mauricio-franciscon",
+    "photo": "../assets/candidatos/mauricio-franciscon.jpg"
+  },
+  {
+    "name": "Naná Vieira",
+    "role": "Deputada Federal",
+    "number": "1412",
+    "slug": "nana-vieira",
+    "photo": "../assets/candidatos/nana-vieira.jpg"
+  },
+  {
+    "name": "Pedro Deyrot",
+    "role": "Deputado Federal",
+    "number": "1414",
+    "slug": "pedro-deyrot",
+    "photo": "../assets/candidatos/pedro-deyrot.jpg"
+  },
+  {
+    "name": "Prof. This",
+    "role": "Deputada Federal",
+    "number": "1426",
+    "slug": "prof-this",
+    "photo": "../assets/candidatos/jessica-this-do-autismo.jpg"
+  },
+  {
+    "name": "Prof. Yan Gaudard",
+    "role": "Deputado Federal",
+    "number": "1441",
+    "slug": "prof-yan-gaudard",
+    "photo": "../assets/candidatos/prof-yan-gaudard.jpg"
+  },
+  {
+    "name": "Ricardo Cascavel Mais Segura",
+    "role": "Deputado Federal",
+    "number": "1445",
+    "slug": "ricardo-cascavel-mais-segura",
+    "photo": "../assets/candidatos/ricardo-cascavel-mais-segura.jpg"
+  },
+  {
+    "name": "Soldado Guedes",
+    "role": "Deputado Federal",
+    "number": "1438",
+    "slug": "soldado-guedes",
+    "photo": "../assets/candidatos/soldado-guedes.jpg"
+  },
+  {
+    "name": "Victor Moreira",
+    "role": "Deputado Federal",
+    "number": "1488",
+    "slug": "victor-moreira",
+    "photo": "../assets/candidatos/victor-moreira.jpg"
+  },
+  {
+    "name": "Luiz França",
+    "role": "Governador",
+    "number": "14",
+    "slug": "luiz-franca",
+    "photo": "../assets/candidatos/luiz-franca.jpg"
+  },
+  {
+    "name": "Karen Guerreiro",
+    "role": "Senadora",
+    "number": "144",
+    "slug": "karen-guerreiro",
+    "photo": "../assets/candidatos/karen-guerreiro.jpg"
+  }
+];
